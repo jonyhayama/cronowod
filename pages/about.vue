a7c2cbe9b679d8331d5355c67a984c1dfdb4e2b2
Jony Hayama <jonathan.hayama@gmail.com>
2024-07-18 14:44:25 -0300

    💬 Update About page content

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="page-wrapper">
     <h1>About</h1>
-    <p>App built to help out during Workout session</p>
+    <p>App built to help out during your Workout session.</p>
+    <p>Feel free to use it as you see fit.</p>
+    <p>This app is open-source and provided "as is", don't blame me if you can't finish your workout 😅</p>
   </div>
 </template>
