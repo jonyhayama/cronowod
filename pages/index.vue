27705902cd686cacc96a8bfdd4a10fe690333901
Jony Hayama <jonathan.hayama@gmail.com>
2024-07-18 04:47:13 -0300

    ✨ List Workouts on Home

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { useStorage } from '@vueuse/core'
+
+const workouts = useStorage('cronowod/workouts', {
+  v: "0.1",
+  items: []
+}, localStorage, { mergeDefaults: true });
+</script>
+
 <template>
-  <h1>Home</h1>
+  <div v-if="workouts.items.length <= 0">
+    <p>No workouts found</p>
+    <Button @click="navigateTo('/workout')" label="Create Workout" />
+  </div>
+  <Tabs v-else :value="workouts.items[0].id">
+    <TabList>
+      <Tab v-for="workout in workouts.items" :value="workout.id">{{ workout.name }}</Tab>
+    </TabList>
+    <TabPanels>
+      <TabPanel v-for="workout in workouts.items" :value="workout.id">
+        <div style="white-space: pre; font-size: 3.5em; line-height: 1em;">
+          {{ workout.description }}
+        </div>
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
 </template>
