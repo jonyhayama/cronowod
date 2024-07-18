cf97d4f9e142d2cc74bf3126c83fd86fc1125744
Jony Hayama <jonathan.hayama@gmail.com>
2024-07-18 14:39:19 -0300

    💄 Add padding around some pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,6 @@
 <template>
-  <h1>About</h1>
-  <p>App built to help out during Workout session</p>
+  <div class="page-wrapper">
+    <h1>About</h1>
+    <p>App built to help out during Workout session</p>
+  </div>
 </template>
--- pages/workouts.vue
+++ pages/workouts.vue
@@ -81,42 +81,44 @@ const saveWorkout = () => {
 </script>
 
 <template>
-  <h1>Workouts</h1>
-  
-  <Button v-if="workouts.items.length < maxWorkouts" label="New" icon="pi pi-plus" @click="openNew" />
-  <div v-else>You can only add 5 workouts.</div>
+  <div class="page-wrapper">
+    <h1>Workouts</h1>
+    
+    <Button v-if="workouts.items.length < maxWorkouts" label="New" icon="pi pi-plus" @click="openNew" />
+    <div v-else>You can only add 5 workouts.</div>
 
-  <DataTable :value="workouts.items" editMode="row">
-    <Column field="name" header="Name" />
-    <Column field="description" header="Description">
-      <template #body="{ data }">
-        <div style="white-space: pre;">{{ data.description }}</div>
-      </template>
-    </Column>
-    <Column>
-      <template #body="slotProps">
-        <div style="display: flex; gap: 0.5rem;">
-          <Button icon="pi pi-pencil" outlined rounded @click="editWorkout(slotProps.data)" />
-          <Button icon="pi pi-trash" outlined rounded severity="danger" @click="deleteWorkout(slotProps.data)" />
-        </div>
-      </template>
-    </Column>
-  </DataTable>
+    <DataTable :value="workouts.items" editMode="row">
+      <Column field="name" header="Name" />
+      <Column field="description" header="Description">
+        <template #body="{ data }">
+          <div style="white-space: pre;">{{ data.description }}</div>
+        </template>
+      </Column>
+      <Column>
+        <template #body="slotProps">
+          <div style="display: flex; gap: 0.5rem;">
+            <Button icon="pi pi-pencil" outlined rounded @click="editWorkout(slotProps.data)" />
+            <Button icon="pi pi-trash" outlined rounded severity="danger" @click="deleteWorkout(slotProps.data)" />
+          </div>
+        </template>
+      </Column>
+    </DataTable>
 
-  <Dialog v-model:visible="workoutDialog" :style="{ width: '450px' }" header="Workout Details" :modal="true">
-    <div>
-      <label for="name" class="block font-bold mb-3">Name</label>
-      <InputText id="name" v-model.trim="workout.name" required="true" autofocus :invalid="submitted && !workout.name" fluid />
-      <small v-if="submitted && !workout.name" class="text-red-500">Name is required.</small>
-    </div>
-    <div>
-      <label for="description" class="block font-bold mb-3">Description</label>
-      <Textarea id="description" v-model="workout.description" required="true" rows="8" cols="20" :invalid="submitted && !workout.description" fluid style="resize: none"/>
-      <small v-if="submitted && !workout.description" class="text-red-500">Description is required.</small>
-    </div>
-    <template #footer>
-      <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
-      <Button label="Save" icon="pi pi-check" @click="saveWorkout" />
-    </template>
-  </Dialog>
+    <Dialog v-model:visible="workoutDialog" :style="{ width: '450px' }" header="Workout Details" :modal="true">
+      <div>
+        <label for="name" class="block font-bold mb-3">Name</label>
+        <InputText id="name" v-model.trim="workout.name" required="true" autofocus :invalid="submitted && !workout.name" fluid />
+        <small v-if="submitted && !workout.name" class="text-red-500">Name is required.</small>
+      </div>
+      <div>
+        <label for="description" class="block font-bold mb-3">Description</label>
+        <Textarea id="description" v-model="workout.description" required="true" rows="8" cols="20" :invalid="submitted && !workout.description" fluid style="resize: none"/>
+        <small v-if="submitted && !workout.description" class="text-red-500">Description is required.</small>
+      </div>
+      <template #footer>
+        <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
+        <Button label="Save" icon="pi pi-check" @click="saveWorkout" />
+      </template>
+    </Dialog>
+  </div>
 </template>
