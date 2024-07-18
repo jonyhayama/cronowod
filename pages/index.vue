25e5cd090abe6c2331d55b75cdc17d41bf36c963
Jony Hayama <jonathan.hayama@gmail.com>
2024-07-18 14:12:38 -0300

    ✨ Allow users to resize font

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,10 @@ const workouts = useStorage('cronowod/workouts', {
   v: "0.1",
   items: []
 }, localStorage, { mergeDefaults: true });
+
+const fontSize = ref(3.5);
+const increaseFontSize = () => { fontSize.value += 0.5; };
+const decreaseFontSize = () => { fontSize.value -= 0.5; };
 </script>
 
 <template>
@@ -16,11 +20,15 @@ const workouts = useStorage('cronowod/workouts', {
       </div>
       <Tabs v-else :value="workouts.items[0].id">
         <TabList style="position: sticky; top: 0; left: 0; right: 0;">
+          <div style="position: absolute; top: 0.3em; right: 0.3em; z-index: 9; display: flex; gap: 0.5em;">
+            <Button @click="increaseFontSize" icon="pi pi-search-plus" />
+            <Button @click="decreaseFontSize" icon="pi pi-search-minus" />
+          </div>
           <Tab v-for="workout in workouts.items" :value="workout.id">{{ workout.name }}</Tab>
         </TabList>
         <TabPanels>
           <TabPanel v-for="workout in workouts.items" :value="workout.id">
-            <div style="white-space: break-spaces; font-size: 3.5em; line-height: 1em;">
+            <div :style="`white-space: break-spaces; font-size: ${fontSize}em; line-height: 1em;`">
               {{ workout.description }}
             </div>
           </TabPanel>
